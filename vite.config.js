5e9cd108e351cd511d6ed8c61a1b2748e88bb731
Asyral <asyral777@gmail.com>
2025-09-30 11:24:55 +0000

    [vite]: fix config for production

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { globSync } from 'glob';
-import { readFileSync } from 'node:fs';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { globSync } from "glob";
+import { readFileSync, existsSync } from "node:fs";
+
+const sslKeyPath = "/home/vlasusu/ssl/mimod-bo/privkey.pem";
+const sslCertPath = "/home/vlasusu/ssl/mimod-bo/fullchain.pem";
+const hasSSL = existsSync(sslKeyPath) && existsSync(sslCertPath);
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                ...globSync('resources/css/**/*.css'),
-                ...globSync('resources/js/**/*.js')
+                ...globSync("resources/css/**/*.css"),
+                ...globSync("resources/js/**/*.js"),
             ],
             refresh: true,
         }),
     ],
-    server: {
-        host: 'mimod-bo.aksarahati.space',
-        https: {
-            key: readFileSync('/home/vlasusu/ssl/mimod-bo/privkey.pem'),
-            cert: readFileSync('/home/vlasusu/ssl/mimod-bo/fullchain.pem'),
+
+    ...(hasSSL && {
+        server: {
+            host: "mimod-bo.aksarahati.space",
+            https: {
+                key: readFileSync("/home/vlasusu/ssl/mimod-bo/privkey.pem"),
+                cert: readFileSync("/home/vlasusu/ssl/mimod-bo/fullchain.pem"),
+            },
+            cors: true,
         },
-        cors: true,
-    }
+    }),
 });
